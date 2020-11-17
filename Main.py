380873b6eeb040856458334431df07d726141161
Eduard Dinu <44479059+Maxim323@users.noreply.github.com>
2020-11-17 14:43:32 +0200

    Autocanny

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import numpy as np
 import cv2
 
-camera = cv2.VideoCapture("parking_test.mp4")
-camera.open("parking_test.mp4")
-
+camera = cv2.VideoCapture("parking_test3.mp4")
+camera.open("parking_test3.mp4")
 
 
 
@@ -17,23 +16,23 @@ while True:
     lowerThreshold = int(max(0, (1.0 - sigma) * median))
     upperThreshold = int(min(255,(1.0 + sigma)*median))
 
-    autocanny = cv2.Canny(frame, lowerThreshold,upperThreshold)
+    # converting to grayScale
+    grayFilter = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    autocanny = cv2.Canny(grayFilter, lowerThreshold,upperThreshold)
     cv2.imshow('Autocanny', autocanny)
 
     #showing original video
     cv2.imshow('CameraFeed',frame)
 
-    #converting to grayScale
-    grayFilter = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     #showing grayScale video
-    cv2.imshow('GrayFilter', grayFilter)
+    #cv2.imshow('GrayFilter', grayFilter)
 
     #cannyEdge detector
     cannyFilter = cv2.Canny(grayFilter, 100, 200)
 
     #showing cannyEdge video
-    cv2.imshow('CannyFilter', cannyFilter)
+    #cv2.imshow('CannyFilter', cannyFilter)
 
     #define exit program key (q)
     if cv2.waitKey(1) & 0xFF == ord('q'):
