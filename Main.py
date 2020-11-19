32085488bcab3bc4e217fbd226f4638d06b834a1
Eduard Dinu <44479059+Maxim323@users.noreply.github.com>
2020-11-19 12:50:39 +0200

    New detection method + new sample video

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-camera = cv2.VideoCapture("parking_test4.mp4")
-camera.open("parking_test4.mp4")
+camera = cv2.VideoCapture("parking_test5.mp4")
+camera.open("parking_test5.mp4")
 
 
 
@@ -10,26 +10,35 @@ camera.open("parking_test4.mp4")
 while True:
     ret, frame = camera.read()
 
+    # define area to be used
+    # x = 12 y = 7
+    # x = 1261 y 816
+    CarSpots = frame[7: 816, 12: 1261]
+
     #autocanny
     sigma = 0.3
-    median = np.median(frame)
+    median = np.median(CarSpots)
     lowerThreshold = int(max(0, (1.0 - sigma) * median))
     upperThreshold = int(min(255,(1.0 + sigma)*median))
 
     # converting to grayScale
-    grayFilter = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayFilter = cv2.cvtColor(CarSpots, cv2.COLOR_BGR2GRAY)
+    autocanny = cv2.Canny(grayFilter, lowerThreshold, upperThreshold)
 
-    autocanny = cv2.Canny(grayFilter, lowerThreshold,upperThreshold)
-    cv2.imshow('Autocanny', autocanny)
+    # binary image
+    _, threshold = cv2.threshold(autocanny, 80, 255, cv2.THRESH_BINARY)
+
+
+
 
     #showing original video
     cv2.imshow('CameraFeed',frame)
-
+    cv2.imshow("BinaryFilter", threshold)
     #showing grayScale video
     #cv2.imshow('GrayFilter', grayFilter)
 
     #cannyEdge detector
-    cannyFilter = cv2.Canny(grayFilter, 100, 200)
+    #cannyFilter = cv2.Canny(grayFilter, 100, 200)
 
     #showing cannyEdge video
     #cv2.imshow('CannyFilter', cannyFilter)
