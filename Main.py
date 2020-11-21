f6c71792b97bdcca46361b65a35cb6c415976146
Eduard Dinu <44479059+Maxim323@users.noreply.github.com>
2020-11-21 19:18:07 +0200

    BinaryFilter and contour detection working properly

--- Main.py
+++ Main.py
@@ -4,68 +4,41 @@ import cv2
 camera = cv2.VideoCapture("parking_test5.mp4")
 camera.open("parking_test5.mp4")
 
-
-
-
 while True:
     ret, frame = camera.read()
 
-    # define area to be used
-    # x = 12 y = 7
-    # x = 1261 y 816
-    CarSpots = frame[7: 816, 12: 1261]
-
-    #autocanny
+    # autocanny
     sigma = 0.3
-    median = np.median(CarSpots)
+    median = np.median(frame)
     lowerThreshold = int(max(0, (1.0 - sigma) * median))
-    upperThreshold = int(min(255,(1.0 + sigma)*median))
+    upperThreshold = int(min(255, (1.0 + sigma) * median))
 
     # converting to grayScale
-    grayFilter = cv2.cvtColor(CarSpots, cv2.COLOR_BGR2GRAY)
+    grayFilter = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     autocanny = cv2.Canny(grayFilter, lowerThreshold, upperThreshold)
 
     # binary image
     _, threshold = cv2.threshold(autocanny, 80, 255, cv2.THRESH_BINARY)
 
-    # trying contour searching
-    # contours, hierarchy = cv2.findContours(threshold,cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    # print("Number of countrs= " + str(len(contours)))
-    #cv2.drawContours(frame, contours, -1, (0,255,0), 1)
+    #binary filter
+    cv2.imshow("Original", threshold)
 
-    #drawing the spots
-    Lot1 = cv2.circle(frame,(195,386),10,(0, 0, 255),1,)
-    Lot2 = cv2.circle(frame,(239,385),10,(0,0,225),1)
-    Lot3 = cv2.circle(frame, (263, 365), 10, (0, 0, 225), 1)
-    Lot4 = cv2.circle(frame, (239, 385), 10, (0, 0, 225), 1)
-    Lot5 = cv2.circle(frame, (298, 353), 10, (0, 0, 225), 1)
-    Lot6 = cv2.circle(frame, (325, 339), 10, (0, 0, 225), 1)
-    Lot7 = cv2.circle(frame, (356, 328), 10, (0, 0, 225), 1)
-    Lot8 = cv2.circle(frame, (385, 314), 10, (0, 0, 225), 1)
-    Lot9 = cv2.circle(frame, (403, 303), 10, (0, 0, 225), 1)
-    Lot10 = cv2.circle(frame, (438, 294), 10, (0, 0, 225), 1)
-    Lot11 = cv2.circle(frame, (468, 279), 10, (0, 0, 225), 1)
+    #incercare detectare diferenta de nr conture
+    frame2 = grayFilter[272:321,366:466]
+    _, threshold2 = cv2.threshold(frame2, 80, 255, cv2.THRESH_BINARY)
 
-    for contour in Lot1:
-        contours = cv2.findContours(Lot1, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        cv2.imshow(contours)
+    cv2.imshow("Frame2", threshold2)
 
-        if cv2.contourArea(contours) < 100:
-            continue
-            cv2.putText(frame, "Status")
+    contours, hierarchy= cv2.findContours(threshold2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        (x,y,w,h) = cv2.boundingRect(cnt)
+
+        area = cv2.contourArea(cnt)
+
+        if area < 4752: print("Loc ocupat!")
+        elif area >= 4752: print("Loc liber")
 
 
-    #showing original video
-    #cv2.imshow('CameraFeed',frame)
-    #cv2.imshow("BinaryFilter", threshold)
-    #showing grayScale video
-    #cv2.imshow('GrayFilter', grayFilter)
-
-    #cannyEdge detector
-    #cannyFilter = cv2.Canny(grayFilter, 100, 200)
-
-    #showing cannyEdge video
-    #cv2.imshow('CannyFilter', cannyFilter)
 
     #define exit program key (q)
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- MotionDetection.py
+++ MotionDetection.py
@@ -1,45 +1,42 @@
-import cv2
 import numpy as np
+import cv2
 
-cap = cv2.VideoCapture(0)
-frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+camera = cv2.VideoCapture("parking_test5.mp4")
+camera.open("parking_test5.mp4")
 
-frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
 
-fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+while True:
+    ret, frame = camera.read()
 
-out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
+    grayFilter = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
-print(frame1.shape)
-while cap.isOpened():
-    diff = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+ #incercare detectare diferenta de nr conture
+    frame2 = grayFilter[272:321,366:466]
+    _, threshold2 = cv2.threshold(frame2, 80, 255, cv2.THRESH_BINARY)
 
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
+    cv2.imshow("Frame2", threshold2)
 
-        if cv2.contourArea(contour) < 900:
-            continue
-        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 0, 255), 3)
-    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    contours, hierarchy= cv2.findContours(threshold2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        (x,y,w,h) = cv2.boundingRect(cnt)
 
-    image = cv2.resize(frame1, (1280,720))
-    out.write(image)
-    cv2.imshow("feed", frame1)
-    frame1 = frame2
-    ret, frame2 = cap.read()
+        area = cv2.contourArea(cnt)
+        if area < 4752:
+            print("Loc ocupat!")
+            cv2.rectangle(threshold2,(x,y),(x+w, y+h),(0,0,255),2)
+        elif area >= 4752:
+            print("Loc liber")
+            cv2.rectangle(threshold2, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    if cv2.waitKey(40) == 27:
+
+
+    #define exit program key (q)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
+#release cv2
+camera.release()
 cv2.destroyAllWindows()
-cap.release()
-out.release()
+
+
