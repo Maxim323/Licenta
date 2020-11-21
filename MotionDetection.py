cabf93a23ed1a77e42d8996d3e6d3c46856cb6ed
Eduard Dinu <44479059+Maxim323@users.noreply.github.com>
2020-11-21 17:21:13 +0200

    New method of detection

--- MotionDetection.py
+++ MotionDetection.py
@@ -1,45 +1,39 @@
-import cv2
 import numpy as np
+import cv2
 
-cap = cv2.VideoCapture(0)
-frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+camera = cv2.VideoCapture("parking_test5.mp4")
+camera.open("parking_test5.mp4")
 
-frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
 
-fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
 
-out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
 
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
-print(frame1.shape)
-while cap.isOpened():
-    diff = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+while True:
+    ret, frame = camera.read()
 
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
+    # drawing the spots
+    Lot1 = cv2.circle(frame, (195, 386), 10, (0, 0, 255), 1, )
+    Lot2 = cv2.circle(frame, (239, 385), 10, (0, 0, 225), 1)
+    Lot3 = cv2.circle(frame, (263, 365), 10, (0, 0, 225), 1)
+    Lot4 = cv2.circle(frame, (239, 385), 10, (0, 0, 225), 1)
+    Lot5 = cv2.circle(frame, (298, 353), 10, (0, 0, 225), 1)
+    Lot6 = cv2.circle(frame, (325, 339), 10, (0, 0, 225), 1)
+    Lot7 = cv2.circle(frame, (356, 328), 10, (0, 0, 225), 1)
+    Lot8 = cv2.circle(frame, (385, 314), 10, (0, 0, 225), 1)
+    Lot9 = cv2.circle(frame, (403, 303), 10, (0, 0, 225), 1)
+    Lot10 = cv2.circle(frame, (438, 294), 10, (0, 0, 225), 1)
+    Lot11 = cv2.circle(frame, (468, 279), 10, (0, 0, 225), 1)
 
-        if cv2.contourArea(contour) < 900:
-            continue
-        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 0, 255), 3)
-    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    cv2.imshow("Frame", frame)
+    
 
-    image = cv2.resize(frame1, (1280,720))
-    out.write(image)
-    cv2.imshow("feed", frame1)
-    frame1 = frame2
-    ret, frame2 = cap.read()
 
-    if cv2.waitKey(40) == 27:
+    #define exit program key (q)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
+#release cv2
+camera.release()
 cv2.destroyAllWindows()
-cap.release()
-out.release()
+
+
